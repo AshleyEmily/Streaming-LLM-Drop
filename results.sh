3de448ff17c11c1a6510bf7eea4015fb6e6f4117
AshleyEmily <ashleyemily2nd@gmail.com>
2026-04-18 19:46:22 -0700

    untrack results.sh, upload.sh, and logs.sh

--- results.sh
+++
@@ -1,5 +0,0 @@
-rsync -av --progress \
-  --exclude='checkpoint' \
-  --exclude='cache' \
-  015902406@coe-hpc1.sjsu.edu:~/results_prune/ \
-  /Users/ashleyirawan/Desktop/thesisv2/forks/LLM-Drop-v2/results_prune/
